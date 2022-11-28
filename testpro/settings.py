bd78c98745ec77f3e65f294408c6f24b6c7bf1f2
Dawde Seifu <93237969+dawaisthename@users.noreply.github.com>
2022-11-28 16:07:17 +0300

    setting updated

--- testpro/settings.py
+++ testpro/settings.py
@@ -117,8 +117,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'images/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static'
+]
+MEDIA_ROOT = os.path.join(BASE_DIR,'staticc/images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
